da58136fdbcb7bf05b8cda584445f986829e6223
FatherAntelope <43047846+FatherAntilopi@users.noreply.github.com>
2021-08-01 00:12:11 +0300

    Исправлена ошибка со скроллингом

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ let navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener('click', () => {
     hideSection();
     toggleNavbar();
-    document.body.classList.toggle("hide-scrolling");
+    document.body.classList.toggle("hide-scroll");
 });
 
 /**----------------Активация секций---------------**/
@@ -26,13 +26,13 @@ document.addEventListener("click", evt => {
             toggleNavbar();
         } else {
             hideSection();
-            document.body.classList.add("hide-scrolling");
+            document.body.classList.add("hide-scroll");
         }
         setTimeout(() => {
             document.querySelector("section.active").classList.remove("active", "fade-out");
             document.querySelector(evt.target.hash).classList.add("active");
             window.scrollTo(0, 0);
-            document.body.classList.remove("hide-scrolling");
+            document.body.classList.remove("hide-scroll");
             navToggler.classList.remove("hide");
             document.querySelector(".overlay").classList.remove("active");
         }, 500)
